7e48bc021b2a804d0c836174ea1fc75ac2cac5d5
jun <19s01724@gmail.com>
2020-07-08 11:17:22 +0900

    内容の書き換え

--- thread.php
+++ thread.php
@@ -6,15 +6,30 @@ class Thread
 {
     private $name;
     private const THREAD_FILE = 'thread.txt';
+    private $dbh;
 
     public function __construct(string $name)
     {
         $this->name = $name;
+        $this->dbh = new PDO('mysql:dbname=thread;host=127.0.0.1', 'root', 'password');
     }
 
     public function getList()
     {
-        return file_get_contents(self::THREAD_FILE);
+        $res = "";
+        $stmt = $this->dbh->query("SELECT * FROM `thread` WHERE `deleted_at` IS NULL ORDER BY `created_at` ASC");
+        while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+            $created_at = $row["created_at"];
+            $name = $row["name"];
+            $contents = $row["content"];
+            $res .= "<hr>\n";
+            $res .= "<p>投稿日時: ".date("Y/m/d H:i:s", strtotime($created_at))."</p>\n";
+            $res .= "<p>投稿者:".$name."</p>\n";
+            $res .= "<p>内容:</p>\n";
+            $res .= "<p>".nl2br($contents)."</p>\n";
+        }
+
+        return $res;
     }
 
     public function post(string $personal_name, string $contents)
@@ -22,13 +37,13 @@ class Thread
         $data = "<hr>\n";
         $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
         $data = $data."<p>投稿者:".$personal_name."</p>\n";
-        $data = $data."<p>内容:</p>";
-        $data = $data."<p>".$contents."</p>";
+        $data = $data."<p>内容:</p>\n";
+        $data = $data."<p>".$contents."</p>\n";
 
         file_put_contents(self::THREAD_FILE, $data, FILE_APPEND);
     }
 
     public function delete() {
-        file_put_contents(self::THREAD_FILE,"");
+        file_put_contents(self::THREAD_FILE, "");
     }
 }
